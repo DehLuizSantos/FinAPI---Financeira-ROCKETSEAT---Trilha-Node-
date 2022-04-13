d3401b3396a02bd86b5baf424ff1fd105fe1b95d
DehLuizSantos <and_consul@outlook.com>
2022-04-13 12:41:31 -0300

    feature(create&statement):routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,49 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+console.log(customers);
+
+/* 
+    CPF: String/
+    Name: String/
+    Id - uuid
+    statement []
+*/
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Custumer already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
+  const customer = customers.find((custumer) => custumer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  return response.json(customer.statement);
+});
 
 app.listen(3333);
