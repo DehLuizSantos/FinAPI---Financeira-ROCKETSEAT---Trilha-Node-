75b92fd6b3bc05975d214cd118cca5da03ee3057
DehLuizSantos <and_consul@outlook.com>
2022-04-20 12:12:20 -0300

    feature(all-files):app-fim

--- README.md
+++ README.md
@@ -1,21 +1,24 @@
 ##FinAPI - Financeira
 
 Requisitos
+
 [X] Deve ser possível criar uma conta
 
 [X] Deve ser possível buscar o extrato bancário do cliente
 
-[] Deve ser possível realizar um depósito
+[X] Deve ser possível realizar um depósito
 
-[] Deve ser possível realizar um saque
+[X] Deve ser possível realizar um saque
 
-[] Deve ser possível buscar o extrato bancário do cliente por data
+[X] Deve ser possível buscar o extrato bancário do cliente por data
 
-[] Deve ser possível atualizar dados da conta do cliente
+[X] Deve ser possível atualizar dados da conta do cliente
 
-[] Deve ser possível obter dados da conta do cliente
+[X] Deve ser possível obter dados da conta do cliente
 
-[] Deve ser possível deletar uma conta
+[X] Deve ser possível deletar uma conta
+
+[X] Deve ser possível retornar o balanço da conta
 
 Regras de negócio
 
@@ -23,12 +26,12 @@ Regras de negócio
 
 [X] Não deve ser possível fazer depósito em uma conta não existente
 
-[] Não deve ser possível cadastrar uma conta com CPF já existente
+[X] Não deve ser possível cadastrar uma conta com CPF já existente
 
-[] Não deve ser possível fazer depósito em uma conta não existente
+[X] Não deve ser possível fazer depósito em uma conta não existente
 
-[] Não deve ser possível fazer saque em uma conta não existente
+[X] Não deve ser possível fazer saque em uma conta não existente
 
-[] Não deve ser possível excluir uma conta não existente
+[X] Não deve ser possível excluir uma conta não existente
 
-[] Não deve ser possível fazer saque quando o saldo for insuficiente
+[X] Não deve ser possível fazer saque quando o saldo for insuficiente
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -8,12 +9,29 @@ app.use(express.json());
 const customers = [];
 console.log(customers);
 
-/* 
-    CPF: String/
-    Name: String/
-    Id - uuid
-    statement []
-*/
+//Middleware
+function verifyIfExistAccoruntCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+  return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -35,15 +53,87 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
-  const customer = customers.find((custumer) => custumer.cpf === cpf);
+//app.use(verifyIfExistAccoruntCPF)
 
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found" });
-  }
-
+app.get("/statement", verifyIfExistAccoruntCPF, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
 });
 
+app.post("/deposit", verifyIfExistAccoruntCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistAccoruntCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insuficient funds!" });
+  }
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExistAccoruntCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dataFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dataFormat).toDateString()
+  );
+
+  return response.json(statement);
+});
+
+app.put("/account", verifyIfExistAccoruntCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+  return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistAccoruntCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistAccoruntCPF, (request, response) => {
+  const { customer } = request;
+
+  //splice (remove a 1 posição, no caso o array inteiro)
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistAccoruntCPF, (request, response) => {
+  const { customer } = request;
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
+});
 app.listen(3333);
